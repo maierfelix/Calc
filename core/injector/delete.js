45f27d99d1cdb0f642ce5ba7fbc27f66158d18eb
Felix <maier.felix96@gmail.com>
2015-06-01 23:56:36 +0200

    Adding injector plugin files

---
+++ core/injector/delete.js
@@ -0,0 +1,38 @@
+/**
+ * This file is part of the NovaeCalc project.
+ *
+ * It is permitted to use, redistribute and/or modify this software
+ * under the terms of the MIT License
+ *
+ * @author Felix Maier <maier.felix96@gmail.com>
+ * @copyright (c) 2015 Felix Maier, @felixmaier
+ *
+ * You may not change or remove these lines
+ *
+ */
+
+"use strict";
+
+  /**
+   * Delete a specific column
+   *
+   * @method deleteColumn
+   * @static
+   */
+  CORE.Injector.prototype.deleteColumn = function(name) {
+
+    
+
+  };
+
+  /**
+   * Delete a specific row
+   *
+   * @method deleteRow
+   * @static
+   */
+  CORE.Injector.prototype.deleteRow = function(name) {
+
+    
+
+  };
---
+++ core/injector/insert.js
@@ -0,0 +1,38 @@
+/**
+ * This file is part of the NovaeCalc project.
+ *
+ * It is permitted to use, redistribute and/or modify this software
+ * under the terms of the MIT License
+ *
+ * @author Felix Maier <maier.felix96@gmail.com>
+ * @copyright (c) 2015 Felix Maier, @felixmaier
+ *
+ * You may not change or remove these lines
+ *
+ */
+
+"use strict";
+
+  /**
+   * Insert a column before another
+   *
+   * @method insertColumn
+   * @static
+   */
+  CORE.Injector.prototype.insertColumn = function(name) {
+
+    
+
+  };
+
+  /**
+   * Insert a row before another
+   *
+   * @method insertRow
+   * @static
+   */
+  CORE.Injector.prototype.insertRow = function(name) {
+
+    
+
+  };
